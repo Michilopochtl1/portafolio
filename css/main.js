40bd5c5ce6db8be3d79a3e28fd829843401cd611
Alma Graciela Martínez González <bbeyday@gmail.com>
2023-11-20 23:28:12 -0600

    Se agregó el carrusel

--- css/main.js
+++ css/main.js
@@ -7,4 +7,27 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
   
+
   
+//   --------------------CARRUSEL--------------------
+
+
+
+$('.slider').slick({
+    centerMode: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        Infinity: true,
+        slidesToShow: 1
+      },
+    }]
+  });
